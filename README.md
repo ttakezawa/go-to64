65468ef337ad605b677d6ca4784f6221b01aff8d
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2021-10-25 02:05:07 +0900

    Import

---
+++ README.md
@@ -0,0 +1,97 @@
+# go-to64
+
+## About
+
+**go-to64** analyzes Golang main package to convert `int`/`uint` to `int64`/`uint64`. This is an experiment tool, so be very careful.
+
+In a 32-bit environment such as CodeForces, this tool may be useful if you want to force the source code of main package to always use 64-bit integers.
+
+## Installation
+
+```bash
+go install github.com/ttakezawa/go-to64
+```
+
+## Usage
+
+```bash
+go-to64 -fix main.go
+```
+
+## Experimental result
+
+This tool will replace type and cast integer literal to int64 or uint64 as shown in the following example.
+
+Before:
+
+```go
+package main
+
+import (
+	"fmt"
+	"math/bits"
+)
+
+const (
+	A, B int  = 1, 2
+	C    uint = 3
+	D         = 4
+	E         = 5 + int(C)
+)
+
+var (
+	a, b int  = 1, 2
+	c    uint = 3
+	d         = 4
+	e         = 5 + add(6+a, int(c))
+)
+
+func main() {
+	s := 15
+	s = bits.OnesCount(uint(s))
+	for i := 0; i < add(a, b); i++ {
+		fmt.Println(i + 100 + add(a, b))
+	}
+}
+
+func add(a, b int) int {
+	return a + b
+}
+```
+
+After:
+
+```go
+package main
+
+import (
+	"fmt"
+	"math/bits"
+)
+
+const (
+	A, B int64  = 1, 2
+	C    uint64 = 3
+	D           = 4
+	E           = 5 + int64(C)
+)
+
+var (
+	a, b int64  = 1, 2
+	c    uint64 = 3
+	d           = int64(4)
+	e           = int64(5) + add(int64(6)+a, int64(c))
+)
+
+func main() {
+	s := int64(15)
+	s = int64(bits.OnesCount64(uint64(s)))
+	for i := int64(0); i < add(a, b); i++ {
+		fmt.Println(i + int64(100) + add(a, b))
+	}
+}
+
+func add(a, b int64) int64 {
+	return a + b
+}
+```
---
+++ main.go
@@ -0,0 +1,213 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/token"
+
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/singlechecker"
+)
+
+func main() {
+	singlechecker.Main(Analyzer)
+}
+
+const Doc = `Convert int/uint to int64/uint64.`
+
+var Analyzer = &analysis.Analyzer{
+	Name:             "convertTo64bit",
+	Doc:              Doc,
+	Run:              run,
+	RunDespiteErrors: true,
+	FactTypes:        []analysis.Fact{new(noReturn)},
+}
+
+var skipLiteral = false
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	var fn func(n ast.Node) bool
+	fn = func(n ast.Node) bool {
+		if decl, ok := n.(*ast.GenDecl); ok {
+			switch decl.Tok {
+			case token.VAR, token.CONST:
+				for _, spec := range decl.Specs {
+					if valueSpec, ok := spec.(*ast.ValueSpec); ok {
+						if valueSpec.Type != nil {
+							if typeIdent, ok := valueSpec.Type.(*ast.Ident); ok && typeIdent.Name != "" {
+								if typeIdent.Name == "int" || typeIdent.Name == "uint" {
+									msg := fmt.Sprintf("Fix %s -> %s64 in %s declaration", typeIdent.Name, typeIdent.Name, decl.Tok)
+									pass.Report(analysis.Diagnostic{
+										Pos:     typeIdent.Pos(),
+										End:     typeIdent.End(),
+										Message: msg,
+										SuggestedFixes: []analysis.SuggestedFix{{
+											Message: msg,
+											TextEdits: []analysis.TextEdit{{
+												Pos:     typeIdent.Pos(),
+												End:     typeIdent.End(),
+												NewText: []byte(fmt.Sprintf("%s64", typeIdent.Name)),
+											}},
+										}},
+									})
+								}
+							}
+							// If a type is specified in lhs, don't fix constants in rhs
+							skipLiteral = true
+							for _, value := range valueSpec.Values {
+								ast.Inspect(value, fn)
+							}
+							skipLiteral = false
+						} else {
+							// If it's CONST && a type is not specified, don't fix constants in rhs
+							if decl.Tok == token.CONST {
+								skipLiteral = true
+								for _, value := range valueSpec.Values {
+									ast.Inspect(value, fn)
+								}
+								skipLiteral = false
+							} else {
+								// Else, fix normally in rhs
+								for _, value := range valueSpec.Values {
+									ast.Inspect(value, fn)
+								}
+							}
+						}
+					}
+				}
+				return false
+			default:
+				return true
+			}
+		}
+
+		if callFn, ok := n.(*ast.CallExpr); ok {
+			if ident, ok := callFn.Fun.(*ast.Ident); ok {
+				switch ident.Name {
+				case "int64":
+					return false
+
+				case "len": // Convert return value of builtin len
+					msg := fmt.Sprintf("Fix len(...) -> int64(len(...))")
+					pass.Report(analysis.Diagnostic{
+						Pos:     callFn.Pos(),
+						End:     callFn.End(),
+						Message: msg,
+						SuggestedFixes: []analysis.SuggestedFix{{
+							Message: msg,
+							TextEdits: []analysis.TextEdit{
+								{
+									Pos:     callFn.Pos(),
+									End:     callFn.Pos(),
+									NewText: []byte("int64("),
+								},
+								{
+									Pos:     callFn.End(),
+									End:     callFn.End(),
+									NewText: []byte(")"),
+								},
+							},
+						}},
+					})
+					return false
+				}
+			}
+
+			if fun, ok := callFn.Fun.(*ast.SelectorExpr); ok {
+				if pkgIdent, ok := fun.X.(*ast.Ident); ok {
+					switch {
+					case pkgIdent.Name == "bits" && fun.Sel.Name == "OnesCount":
+						msg := fmt.Sprintf("Fix bits.OnesCount(...) -> int64(bits.OnesCount64(...))")
+						pass.Report(analysis.Diagnostic{
+							Pos:     fun.Pos(),
+							End:     fun.End(),
+							Message: msg,
+							SuggestedFixes: []analysis.SuggestedFix{
+								{
+									Message: msg,
+									TextEdits: []analysis.TextEdit{
+										{
+											Pos:     callFn.Pos(),
+											End:     callFn.Pos(),
+											NewText: []byte("int64("),
+										},
+										{
+											Pos:     callFn.End(),
+											End:     callFn.End(),
+											NewText: []byte(")"),
+										},
+										{
+											Pos:     fun.Sel.Pos(),
+											End:     fun.Sel.End(),
+											NewText: []byte("OnesCount64"),
+										},
+									},
+								},
+							},
+						})
+						return true
+					}
+				}
+			}
+		}
+
+		if !skipLiteral {
+			if literal, ok := n.(*ast.BasicLit); ok && literal.Kind == token.INT {
+				msg := fmt.Sprintf("Fix literal %s -> int64(%s)", literal.Value, literal.Value)
+				pass.Report(analysis.Diagnostic{
+					Pos:     literal.Pos(),
+					End:     literal.End(),
+					Message: msg,
+					SuggestedFixes: []analysis.SuggestedFix{{
+						Message: msg,
+						TextEdits: []analysis.TextEdit{{
+							Pos:     literal.Pos(),
+							End:     literal.End(),
+							NewText: []byte(fmt.Sprintf("int64(%s)", literal.Value)),
+						}},
+					}},
+				})
+				return true
+			}
+		}
+
+		// replace int/uint with int64/uint64
+		if ident, ok := n.(*ast.Ident); ok {
+			switch ident.Name {
+			case "int", "uint":
+				msg := fmt.Sprintf("Fix %s -> %s64", ident.Name, ident.Name)
+				pass.Report(analysis.Diagnostic{
+					Pos:     ident.Pos(),
+					End:     ident.End(),
+					Message: msg,
+					SuggestedFixes: []analysis.SuggestedFix{{
+						Message: msg,
+						TextEdits: []analysis.TextEdit{{
+							Pos:     ident.Pos(),
+							End:     ident.End(),
+							NewText: []byte(fmt.Sprintf("%s64", ident.Name)),
+						}},
+					}},
+				})
+				return true
+			}
+		}
+
+		return true
+	}
+
+	for _, f := range pass.Files {
+		if f.Name.Name != "main" {
+			continue
+		}
+		ast.Inspect(f, fn)
+	}
+	return nil, nil
+}
+
+// noReturn is a fact indicating that a function does not return.
+type noReturn struct{}
+
+func (*noReturn) AFact() {}
+
+func (*noReturn) String() string { return "noReturn" }
